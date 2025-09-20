0f2139288026cee5c994fcbbf7857c791a16aca5
YarHoly <yaroslav-skovpen1999@mail.ru>
2025-09-20 15:18:14 +0500

    Add files via upload

---
+++ Project_Wholesale_Store/app/models.py
@@ -0,0 +1,20 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
